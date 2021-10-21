216bd4d6192411b4cb7d0386e4ccdcfaa7b75540
damaestro165 <damaestro165@gmail.com>
2021-10-21 11:18:41 +0100

    Completed module

--- js/kanban.js
+++ js/kanban.js
@@ -51,9 +51,6 @@ document.querySelectorAll('.drop').forEach(element => {
     event.preventDefault();
     const id = event.dataTransfer.getData('text');
     event.target.appendChild(document.getElementById(id));
-
-
   });
-  element.addEventListener('dragover', event =>
-  event.preventDefualt());
+ element.addEventListener('dragover', event => event.preventDefault());
 });
